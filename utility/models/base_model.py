03f2c2def687906fa328002408ca548a5bff08f0
Muflone <webreg@vbsimple.net>
2019-10-14 00:46:46 +0200

    Added base model for every model

--- utility/models/base_model.py
+++ utility/models/base_model.py
@@ -18,4 +18,23 @@
 #  along with this program.  If not, see <https://www.gnu.org/licenses/>.
 ##
 
-from .base_model import BaseModel, BaseModelAdmin                 # noqa: F401
+from django.contrib import admin
+from django.db import models
+
+
+class BaseModel(models.Model):
+    def __init__(self, *args, **kwargs):
+        """Base model for each other model in the application"""
+        super().__init__(*args, **kwargs)
+
+    class Meta:
+        abstract = True
+
+
+class BaseModelAdmin(admin.ModelAdmin):
+    def __init__(self, model, admin_site):
+        """Base Admin model for each other model in the application"""
+        super().__init__(model, admin_site)
+        # If ModelAdmin ordering is missing apply the ordering of the model
+        if not self.ordering:
+            self.ordering = model._meta.ordering

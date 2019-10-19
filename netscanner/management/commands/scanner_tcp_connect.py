d640b1c83d38a20caac969ef402a73180e498f5c
Muflone <webreg@vbsimple.net>
2019-10-19 21:04:34 +0200

    New management command to connect to a TCP port using sockets

---
+++ netscanner/management/commands/scanner_tcp_connect.py
@@ -0,0 +1,87 @@
+##
+#     Project: Django NetScanner
+# Description: A Django application to make network scans
+#      Author: Fabio Castelli (Muflone) <muflone@muflone.com>
+#   Copyright: 2019 Fabio Castelli
+#     License: GPL-3+
+#  This program is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+##
+
+import argparse
+
+from django.utils import timezone
+
+from netscanner.management.management_base_command import ManagementBaseCommand
+from netscanner.models import Discovery, Host
+from netscanner.tools.tcp_connect import TCPConnect
+
+
+class Command(ManagementBaseCommand):
+    help = 'Discover network hosts using TCP connections'
+
+    def __init__(self):
+        super().__init__()
+        self.scanner_tool = 'tcp_connect'
+
+    def add_arguments(self, parser: argparse.ArgumentParser):
+        super().add_arguments(parser)
+        parser.add_argument('--workers',
+                            action='store',
+                            type=int,
+                            default=10)
+        parser.add_argument('--timeout',
+                            action='store',
+                            type=int,
+                            default=1)
+
+    def instance_scanner_tool(self,
+                              options: dict):
+        """
+        Instance the scanner tool using the discovery options
+        :param options: dictionary containing the options
+        :return:
+        """
+        return TCPConnect(timeout=options['timeout'],
+                          portnr=options.get('port', 80))
+
+    def process_results(self,
+                        discovery: Discovery,
+                        results: list) -> None:
+        """
+        Process the results list
+        :param results: list of results to process
+        :return: None
+        """
+        for item in results:
+            (address, values) = item
+            connected = values['connected']
+            if connected:
+                self.print('%-18s %s' % (address, values))
+                # Update last seen time
+                hosts = Host.objects.filter(address=address)
+                if hosts:
+                    # Update existing hosts
+                    for host in hosts:
+                        # Update only if not excluded from discovery
+                        if not host.no_discovery:
+                            host.last_seen = timezone.now()
+                            host.save()
+                else:
+                    # Insert new host
+                    host = Host.objects.create()
+                    host.name = address
+                    host.address = address
+                    host.subnetv4 = discovery.subnetv4
+                    host.last_seen = timezone.now()
+                    host.save()
---
+++ netscanner/tools/tcp_connect.py
@@ -0,0 +1,50 @@
+##
+#     Project: Django NetScanner
+# Description: A Django application to make network scans
+#      Author: Fabio Castelli (Muflone) <muflone@muflone.com>
+#   Copyright: 2019 Fabio Castelli
+#     License: GPL-3+
+#  This program is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+##
+
+import socket
+
+
+class TCPConnect(object):
+    def __init__(self,
+                 timeout: int,
+                 portnr: int):
+        self.timeout = timeout
+        self.portnr = portnr
+
+    def execute(self,
+                destination: str) -> dict:
+        """
+        Connect to an IP address using socket TCP connection
+        """
+        sock = socket.socket(family=socket.AF_INET,
+                             type=socket.SOCK_STREAM,
+                             proto=socket.IPPROTO_TCP)
+        try:
+            sock.settimeout(self.timeout)
+            sock.connect((destination, self.portnr))
+            sock.close()
+            result = True
+        except (ConnectionRefusedError,
+                OSError,
+                socket.timeout):
+            result = False
+        return {
+            'connected': result
+        }

c1cf7501061338283ad3708611d5373ac178b55c
Muflone <webreg@vbsimple.net>
2019-10-30 00:04:20 +0100

    New management command Discovery to launch a specific Discovery

---
+++ netscanner/management/commands/discovery.py
@@ -0,0 +1,75 @@
+##
+#     Project: Django NetScanner
+# Description: A Django application to make network scans
+#      Author: Fabio Castelli (Muflone) <muflone@muflone.com>
+#   Copyright: 2019 Fabio Castelli
+#     License: GPL-3+
+#  This program is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+##
+
+import argparse
+
+from django.core.management.base import BaseCommand
+from django.db import models
+from django.utils.translation import pgettext_lazy
+
+from netscanner.management.discovery_base_command import DiscoveryBaseCommand
+from netscanner.models import Discovery
+
+from . import discovery_tool_commands
+
+
+class Command(BaseCommand):
+    help = 'Custom Discovery'
+
+    def add_arguments(self, parser: argparse.ArgumentParser) -> None:
+        BaseCommand.add_arguments(self, parser)
+        parser.add_argument('--discovery',
+                            action='store',
+                            type=str,
+                            required=True,
+                            help=pgettext_lazy(
+                                'Scanner Custom',
+                                'Discovery to execute'))
+        parser.add_argument('--disabled',
+                            action='store_true',
+                            default=False,
+                            help=pgettext_lazy(
+                                'Scanner Custom',
+                                'Launch also disabled discoveries'))
+
+    def handle(self, *args, **options) -> None:
+        try:
+            discovery = Discovery.objects.get(name=options['discovery'])
+            # Execute only enabled discoveries or any if disabled is passed
+            if discovery.enabled or options['disabled']:
+                # Find the tool for the requested discovery
+                for command in discovery_tool_commands:
+                    if command.tool_name == discovery.scanner.tool:
+                        # Execute discovery for the requested tool
+                        command().do_discovery(
+                            discovery=discovery,
+                            options=DiscoveryBaseCommand().get_options(
+                                general_options={**options},
+                                scanner_options=discovery.scanner.options,
+                                discovery_options=discovery.options))
+                        break
+            else:
+                # Disabled discovery
+                print('The discovery "{NAME}" is disabled'.format(
+                    NAME=discovery.name))
+        except models.ObjectDoesNotExist:
+            # Not existing Discovery
+            print('No discovery named "{NAME}"'.format(
+                NAME=options['discovery']))

a928a9bd593b8c54b53e79aa5a3d1e755cfc53b8
Muflone <webreg@vbsimple.net>
2019-11-03 02:30:13 +0100

    New management command to purge OUIs

---
+++ oui/management/commands/purge_oui.py
@@ -0,0 +1,35 @@
+##
+#     Project: Django NetScanner
+# Description: A Django application to make network scans
+#      Author: Fabio Castelli (Muflone) <muflone@muflone.com>
+#   Copyright: 2019 Fabio Castelli
+#     License: GPL-3+
+#  This program is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+##
+
+import argparse
+
+from django.core.management.base import BaseCommand
+
+from oui.models import Oui
+
+
+class Command(BaseCommand):
+    help = 'Purge vendor OUI'
+
+    def add_arguments(self, parser: argparse.ArgumentParser) -> None:
+        BaseCommand.add_arguments(self, parser)
+
+    def handle(self, *args, **options) -> None:
+        Oui.objects.all().delete()

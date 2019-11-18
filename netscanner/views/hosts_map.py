2db954bb6222752ba7b96321310b6c2bef0917ac
Muflone <webreg@vbsimple.net>
2019-11-18 17:24:25 +0100

    Code cleanup

--- netscanner/views/hosts_map.py
+++ netscanner/views/hosts_map.py
@@ -17,6 +17,7 @@
 #  You should have received a copy of the GNU General Public License
 #  along with this program.  If not, see <https://www.gnu.org/licenses/>.
 ##
+
 from django.views.generic import TemplateView
 
 from netscanner.models import SubnetV4, Host
--- utility/misc/enable_disable_records.py
+++ utility/misc/enable_disable_records.py
@@ -17,6 +17,7 @@
 #  You should have received a copy of the GNU General Public License
 #  along with this program.  If not, see <https://www.gnu.org/licenses/>.
 ##
+
 from django.db.models import QuerySet
 from django.http import HttpResponseRedirect, HttpRequest, HttpResponse
 from django.shortcuts import render
